2cd440a3be80acef395ca758b6f155a0725d355c
Moritz Borcherding <moritz.borcherding@web.de>
2018-11-12 13:04:28 +0100

    added transaction report

--- components/reports.js
+++ components/reports.js
@@ -4,6 +4,7 @@ Vue.component('reports',
             return {
                 accReportHtml: "",
                 bevReportHtml: "",
+                txReportHtml: "",
             }
         },
         template: `
@@ -24,6 +25,16 @@ Vue.component('reports',
     <div v-html=bevReportHtml></div>
     </div>
     </div>
+    <br>
+    <br>
+    <button v-on:click=call_tx_report>Generate TransactionList</button>
+    <br>
+    <br>
+    <div class="row">
+    <div class="col-md-12"> 
+    <div v-html=txReportHtml></div>
+    </div>
+    </div>
     </div>`,
         methods: {
             call_account_report: function () {
@@ -36,6 +47,12 @@ Vue.component('reports',
                 that = this
                 beverageReport(function (report) {
                     that.bevReportHtml = report
+                }, displayError)
+            },
+            call_tx_report: function () {
+                that = this
+                transactionReport("", function (report) {
+                    that.txReportHtml = report
                 }, displayError)
             }
         }
--- hadibarlib/reportCalls.js
+++ hadibarlib/reportCalls.js
@@ -7,3 +7,8 @@ function beverageReport(success, failure){
     isSessionIDSet()
     callApi("/api/f/reports/beverages", 'GET',{}, success, failure)   
 }
+
+function transactionReport(accountID, success, failure){
+    isSessionIDSet()
+    callApi("/api/f/reports/transactions", 'GET',{accid: accountID}, success, failure)   
+}
